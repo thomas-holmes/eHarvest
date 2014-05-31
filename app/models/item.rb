cd77d728ff39a222e133e6b691d385dd0ca34316
kyfast <kylie.stradley@gmail.com>
2014-05-31 12:55:44 -0400

    searching

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ActiveRecord::Base
   belongs_to :item_code
   has_many :order_items
+
+  before_save :to_lower
+
+  def to_lower
+    self.name = self.name.downcase
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140531122739) do
+ActiveRecord::Schema.define(version: 20140531145822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,6 +114,13 @@ ActiveRecord::Schema.define(version: 20140531122739) do
   create_table "reports", force: true do |t|
     t.string   "name"
     t.string   "link"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "searches", force: true do |t|
+    t.string   "names"
+    t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
