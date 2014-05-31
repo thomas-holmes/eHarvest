09a1092f0335780ec843b3fbc59d6c79d8bcc704
Thomas Holmes <thomas@holmes.io>
2014-05-31 11:18:10 -0400

    Add update/destroy interaction between items and order items

--- spec/controllers/order_items_controller_spec.rb
+++ spec/controllers/order_items_controller_spec.rb
@@ -57,6 +57,24 @@ describe OrderItemsController do
           post :create, order_id: order, order_item: FactoryGirl.attributes_for(:order_item, order: order)
         }.to change { order.reload.order_items.count }.by(1)
       end
+
+      it 'copies item information when created' do
+        item = FactoryGirl.create(:item)
+        order_attributes = FactoryGirl.attributes_for(:order_item, order: order, item_id: item.id)
+        post :create, order_id: order, order_item: order_attributes
+
+        order_item = order.reload.order_items.last
+        expect(order_item.fee).to eq item.fee
+        expect(order_item.weight).to eq item.weight
+      end
+
+      it 'decrements stock from item' do
+        item = FactoryGirl.create(:item, quantity: 50)
+        order_attributes = FactoryGirl.attributes_for(:order_item, order: order, quantity: 10, item_id: item.id)
+        post :create, order_id: order, order_item: order_attributes
+        item.reload
+        expect(item.quantity).to eq 40
+      end
     end
 
     context '#update' do
@@ -69,6 +87,24 @@ describe OrderItemsController do
         order_item.reload
         expect(order_item.quantity).to eq(new_qty)
       end
+
+      it 'updates item quantity when quantity is updated' do
+        item = FactoryGirl.create(:item, quantity: 20)
+        order_item = FactoryGirl.create(:order_item, order: order, item: item, quantity: 5)
+
+        put :update, order_id: order, id: order_item.id, order_item: { quantity: 2 }
+
+        expect(item.reload.quantity).to eq 23
+      end
+
+      it 'will fail if new quantity reduces item below 0' do
+        item = FactoryGirl.create(:item, quantity: 10)
+        order_item = FactoryGirl.create(:order_item, order: order, item: item, quantity: 5)
+
+        put :update, order_id: order, id: order_item.id, order_item: { quantity: 20 }, format: :json
+
+        expect(response.status).to be 422
+      end
     end
 
     context '#destroy' do
@@ -78,6 +114,14 @@ describe OrderItemsController do
         delete :destroy, order_id: order, id: order_item, format: :json
 
         expect { OrderItem.find(order_item.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+
+      it 'restores quantity to item when destroyed' do
+        item = FactoryGirl.create(:item, quantity: 10)
+        order_item = FactoryGirl.create(:order_item, order: order, item: item, quantity: 5)
+        delete :destroy, order_id: order, id: order_item, format: :json
+
+        expect(item.reload.quantity).to eq 15
       end
     end
   end
